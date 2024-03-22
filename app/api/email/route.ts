1eb59ede6d6c220aa5f28573618b025d46b71b99
@vcastro-dev <vcastro2395@gmail.com>
2024-03-22 10:47:25 -0300

    feat: desabilitando rota de envio de emails

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,40 +1,40 @@
-import { ContactForm } from '@/components/EmailTemplate';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { Resend } from 'resend';
+// import { ContactForm } from '@/components/EmailTemplate';
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import { Resend } from 'resend';
 
-const RESEND_API_KEY = process.env.RESEND_API_KEY as string;
-const EMAIL_TO = process.env.EMAIL_TO as string;
-const EMAIL_FROM = process.env.EMAIL_FROM as string;
+// const RESEND_API_KEY = process.env.RESEND_API_KEY as string;
+// const EMAIL_TO = process.env.EMAIL_TO as string;
+// const EMAIL_FROM = process.env.EMAIL_FROM as string;
 
-const resend = new Resend(RESEND_API_KEY);
+// const resend = new Resend(RESEND_API_KEY);
 
-type ResponseData = {
-  message: string;
-}
+// type ResponseData = {
+//   message: string;
+// }
 
-export async function POST(req: Request) {
-  try {
-    const { name, email, message } = await req.json();
+// export async function POST(req: Request) {
+//   try {
+//     const { name, email, message } = await req.json();
 
-    if (!name || !email || !message) {
-      throw new Error('Missing required fields');
-    }
+//     if (!name || !email || !message) {
+//       throw new Error('Missing required fields');
+//     }
 
-    const data = await resend.emails.send({
-      from: EMAIL_FROM,
-      to: [EMAIL_TO],
-      subject: 'Um novo contato foi feito pelo site',
-      react: ContactForm({ 
-        name,
-        email,
-        message
-       }) as React.ReactElement,
-    });
+//     const data = await resend.emails.send({
+//       from: EMAIL_FROM,
+//       to: [EMAIL_TO],
+//       subject: 'Um novo contato foi feito pelo site',
+//       react: ContactForm({
+//         name,
+//         email,
+//         message
+//        }) as React.ReactElement,
+//     });
 
-    return Response.json(data, { status: 200 });
-  } catch (error) {
-    if(error instanceof Error) {
-      return Response.json({ message: error.message }, { status: 400 });
-    }
-  }
-}
+//     return Response.json(data, { status: 200 });
+//   } catch (error) {
+//     if(error instanceof Error) {
+//       return Response.json({ message: error.message }, { status: 400 });
+//     }
+//   }
+// }
